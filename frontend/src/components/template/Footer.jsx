cd3a1fd32b2bbd76a1a3f7d3583146f5e054e6c8
galeno <gui.galenocover@gmail.com>
2022-01-31 08:26:46 -0300

    Footer + Nav Components

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -1,7 +1,10 @@
 import './Footer.css'
 import React from 'react'
 
-export default (props) => 
+export default (props) =>
     <footer className="footer">
-        Footer
+        <span>
+            Desenvolvido Com <i className="fa fa-heart text-danger"></i> por
+            <strong> Cod<span className='text-danger'>3</span>r</strong>
+        </span>
     </footer>
