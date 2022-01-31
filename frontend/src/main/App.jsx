c1b4eab9c3c34492f0aeb9a3721d50b5e52cd259
galeno <gui.galenocover@gmail.com>
2022-01-31 08:15:34 -0300

    Header Component

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
 import Logo from '../components/template/Logo'
@@ -5,10 +7,11 @@ import Nav from '../components/template/Nav'
 import Main from '../components/template/Main'
 import Footer from '../components/template/Footer'
 
-export default props => 
+export default props =>
     <div className="app">
         <Logo />
         <Nav />
-        <Main />
+        <Main icon='home' title='Início'
+            subtitle='Segundo projeto do capítulo de React' />
         <Footer />
     </div>
